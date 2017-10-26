cb48f5ce346ccf71345a767aea07a6379aadfe17
redlo <RedLOngCity@gmail.com>
2017-10-26 20:09:15 +0300

    First Commit

---
+++ src/main/java/com/mycompany/springhelloworld/controller/JSONController.java
@@ -0,0 +1,23 @@
+package com.mycompany.springhelloworld.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author redlongcity class for testing
+ */
+@RestController
+@RequestMapping(value="/")
+public class JSONController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public ResponseEntity<String> hello() {
+        String hello = "Hello from Openshift!";
+        return new ResponseEntity<String>(hello, HttpStatus.OK);
+    }
+
+}
